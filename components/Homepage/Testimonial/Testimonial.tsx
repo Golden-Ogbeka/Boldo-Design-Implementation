b399f7a1091920317c74c891fcf279ed49a49910
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2022-09-19 22:57:28 +0100

    Testimonial section

--- components/Homepage/Testimonial/Testimonial.tsx
+++ components/Homepage/Testimonial/Testimonial.tsx
@@ -4,17 +4,34 @@ import ArrowLeft from '../../../public/icons/arrow-left.png';
 import ArrowRight from '../../../public/icons/arrow-right.png';
 import HorizontalSlider from '../../reusable/HorizontalSlider';
 import TestimonialItem from './TestimonialItem';
+import Avatar1 from '../../../public/images/testimonial/avatar-1.png';
+import Avatar2 from '../../../public/images/testimonial/avatar-2.png';
+import Avatar3 from '../../../public/images/testimonial/avatar-3.png';
 
 function Testimonial() {
+	const moveRight = () => {
+		const testimonialSlider: HTMLElement | null =
+			document.getElementById('testimonialItems');
+		if (!!testimonialSlider) {
+			testimonialSlider.scrollLeft += 300;
+		}
+	};
+	const moveLeft = () => {
+		const testimonialSlider: HTMLElement | null =
+			document.getElementById('testimonialItems');
+		if (!!testimonialSlider) {
+			testimonialSlider.scrollLeft -= 300;
+		}
+	};
 	return (
 		<section
 			id='testimonial'
 			className='bg-[#0A2640] pt-[96px] pb-[96px] pl-[5vw] pr-[5vw]'
 		>
 			<div className='reveal'>
-				<div className='flex flex-row flex-wrap gap-10 items-end justify-between mb-[72px]'>
+				<div className='flex flex-row flex-wrap gap-10 items-end lg:justify-between justify-center mb-[72px]'>
 					<h2
-						className='heading max-w-[716px]'
+						className='heading max-w-[716px] text-center lg:text-left'
 						style={{
 							color: '#fff',
 						}}
@@ -22,16 +39,62 @@ function Testimonial() {
 						An enterprise template to ramp up your company website
 					</h2>
 					<div className='flex flex-row items-center gap-7'>
-						<button className='w-[72px] h-[72px] rounded-[36px] bg-white flex items-center justify-center hover:bg-[#65e4a3]'>
+						<button
+							className='w-[72px] h-[72px] rounded-[36px] bg-white flex items-center justify-center hover:bg-[#65e4a3] outline-none border-none'
+							onClick={moveLeft}
+						>
 							<Image src={ArrowLeft} alt='Left' />
 						</button>
-						<button className='w-[72px] h-[72px] rounded-[36px] bg-white flex items-center justify-center hover:bg-[#65e4a3]'>
+						<button
+							className='w-[72px] h-[72px] rounded-[36px] bg-white flex items-center justify-center hover:bg-[#65e4a3] outline-none border-none'
+							onClick={moveRight}
+						>
 							<Image src={ArrowRight} alt='Right' />
 						</button>
 					</div>
 				</div>
-				<HorizontalSlider id='testimonialItems'>
-					<TestimonialItem />
+				<HorizontalSlider
+					id='testimonialItems'
+					style={{
+						alignItems: 'flex-start',
+					}}
+				>
+					<TestimonialItem
+						avatar={Avatar1}
+						name='Albus Dumbledore'
+						position='Manager @ Howarts'
+						testimony='Buyer buzz partner network disruptive non-disclosure agreement business'
+					/>
+					<TestimonialItem
+						avatar={Avatar2}
+						name='Severus Snape'
+						position='Manager @ Slytherin'
+						testimony='Learning curve infrastructure value proposition advisor strategy user experience hypotheses investor.'
+					/>
+					<TestimonialItem
+						avatar={Avatar3}
+						name='Harry Potter'
+						position='Team Leader @ Gryffindor'
+						testimony='Release facebook responsive web design business model canvas seed money monetization.'
+					/>
+					<TestimonialItem
+						avatar={Avatar1}
+						name='Albus Dumbledore'
+						position='Manager @ Howarts'
+						testimony='Buyer buzz partner network disruptive non-disclosure agreement business'
+					/>
+					<TestimonialItem
+						avatar={Avatar2}
+						name='Severus Snape'
+						position='Manager @ Slytherin'
+						testimony='Learning curve infrastructure value proposition advisor strategy user experience hypotheses investor.'
+					/>
+					<TestimonialItem
+						avatar={Avatar3}
+						name='Harry Potter'
+						position='Team Leader @ Gryffindor'
+						testimony='Release facebook responsive web design business model canvas seed money monetization.'
+					/>
 				</HorizontalSlider>
 			</div>
 		</section>
--- components/Homepage/Testimonial/TestimonialItem.tsx
+++ components/Homepage/Testimonial/TestimonialItem.tsx
@@ -1,7 +1,29 @@
+import Image, { StaticImageData } from 'next/image';
 import React from 'react';
 
-function TestimonialItem() {
-	return <div>TestimonialItem</div>;
+function TestimonialItem({
+	testimony,
+	avatar,
+	name,
+	position,
+}: {
+	testimony: string;
+	avatar: StaticImageData;
+	name: string;
+	position: string;
+}) {
+	return (
+		<div className='flex flex-col min-w-[350px] bg-white rounded-[12px] p-10'>
+			<blockquote className='text-2xl'>“{testimony}”</blockquote>
+			<div className='flex flex-row items-center gap-4 mt-10'>
+				<Image src={avatar} alt='Avatar' />
+				<div className='flex flex-col gap-[6px]'>
+					<span className='font-bold'>{name}</span>
+					<span className='text-sm'>{position}</span>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default TestimonialItem;
