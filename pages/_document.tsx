73cb77218ca1a37fe49b353e9f39c8e6972ff78f
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2022-09-19 23:03:25 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
 			<Head>
 				{/* <!-- Fonts --> */}
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
-				<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+				<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
 				{/* Manrope */}
 				<link
 					href='https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap'
